12a550e451b0be6df809cc94e62d931ab17e175a
rachittaneja56-max <rachittaneja56@gmail.com>
2025-12-27 13:40:35 +0530

    Fix CORS preflight by disabling credentials

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
         "http://localhost:5173",   
         "https://rachits-portfolio24.vercel.app/"  
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
